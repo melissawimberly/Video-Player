c7a9dd82e79031aa4565323230d45cba93414b09
melissawimberly <melissa.f.wimberly@gmail.com>
2016-07-28 14:50:49 -0400

    Add SearchBar, VideoList and VideoListItem components

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyDex_awtkg0kMM-WricObPXxSJnxYD9plo';
+
+// Create a new component. This component should produce some HTML
+
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	); 
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
